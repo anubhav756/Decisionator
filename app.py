9a02bd0c908ea6e0f85a4385e470ab585a7f5515
Anubhav Dhawan <anubhavdhawan@google.com>
2024-08-08 17:58:56 +0530

    expose API

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-import asyncio
-
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+
+from script import make_decision
 
 app = FastAPI()
 
 
-@app.post("/ask")
-async def ask_question(question: str):
-    async def stream_response():
-        response_words = []
-        async for word in chain.astream(question):
-            response_words.append(word)
-            yield word
-        final_response = "".join(response_words)
+class Query(BaseModel):
+    query: str
 
-    return StreamingResponse(stream_response(), media_type="text/event-stream")
+
+@app.post("/ask")
+async def ask_question(query: Query):
+    print("GOT QUERY!!!", query)
+    return StreamingResponse(make_decision(query), media_type="text/event-stream")
