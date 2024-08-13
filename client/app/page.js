0f73935b441a598f96e565e0e1136bd302324659
Anubhav Dhawan <anubhavdhawan@google.com>
2024-08-13 14:40:27 +0530

    UI improvements

--- client/app/page.js
+++ client/app/page.js
@@ -6,25 +6,21 @@ const INITIAL_INPUT = '';
 const INITIAL_OPTIONS = [];
 const INITIAL_RESPONSE = {};
 const INITIAL_IS_SUBMITTING = false;
-const INITIAL_IS_OPEN = false;
 
 export default function Page() {
   const [inputText, setInputText] = useState(INITIAL_INPUT);
   const [options, setOptions] = useState(INITIAL_OPTIONS);
   const [response, setResponse] = useState(INITIAL_RESPONSE);
   const [isSubmitting, setIsSubmitting] = useState(INITIAL_IS_SUBMITTING);
-  const [isOpen, setIsOpen] = useState(INITIAL_IS_OPEN);
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
   };
-
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
       handleSnap();
     }
   };
-
   const handleSnap = async () => {
     setResponse(INITIAL_RESPONSE);
     setOptions(INITIAL_OPTIONS);
@@ -65,30 +61,31 @@ export default function Page() {
           onChange={handleInputChange}
           onKeyDown={handleKeyDown}
           placeholder="Type here to decide..."
-          className="w-3/4 p-8 rounded-lg outline-none bg-gray-900 text-white text-5xl resize-none"
+          className="w-3/4 p-8 rounded-full outline-none bg-gray-900 text-white text-5xl resize-none"
         />
-    <div className="relative inline-flex group ml-8">
-      <div className={`absolute transitiona-all duration-1000 bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt ${isSubmitting ? "opacity-100 -inset-1" : "opacity-70 -inset-px"}`} />
-      <a
-        onClick={(e) => {
-          if (isSubmitting || !inputText.length) {
-            e.preventDefault(); 
-          } else {
-            handleSnap(); 
-          }
-        }}
-        className={`relative inline-flex items-center justify-center px-8 py-4 text-5xl text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 focus:outline-none
-                   ${isSubmitting ? 'pointer-events-none opacity-50' : ''}`}
-        role="button">
-        Snap
-      </a>
-    </div>
-    </div>
-    <div>
+        <div className="relative inline-flex group ml-8">
+          <div className={`absolute transitiona-all duration-1000 bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt ${isSubmitting ? "opacity-100 -inset-1" : "opacity-70 -inset-px"}`} />
+          <a
+            onClick={(e) => {
+              if (isSubmitting || !inputText.length) {
+                e.preventDefault(); 
+              } else {
+                handleSnap(); 
+              }
+            }}
+            className={`relative inline-flex items-center justify-center px-8 py-4 text-5xl text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 focus:outline-none
+                      ${isSubmitting ? 'pointer-events-none opacity-50' : ''}`}
+            role="button"
+          >
+            Snap
+          </a>
+        </div>
+      </div>
+      <div>
       <div className="flex justify-center items-center text-4xl mt-24"> 
         <div className="grid grid-cols-2 gap-4"> 
           {options.map((str, index) => (
-            <div key={index} className="flex p-4 rounded-lg shadow-md text-white"> 
+            <div key={index} className="flex justify-center items-center p-4 rounded-lg shadow-md text-white w-full">
               {str}
             </div>
           ))}
@@ -96,38 +93,25 @@ export default function Page() {
       </div>
     </div>
     {response.response ? (
-      <div className="mt-24 justify-center text-3xl transition-all duration-200">
+      <div className="mt-24 justify-center text-3xl">
         <div className="text-gray-400">
           **{response.character} snaps finger**
         </div>
         <div className="text-white mt-4">
           {response.response}
         </div>
-        <button 
-          onClick={() => setIsOpen(true)}
-          className="mt-24 bg-gray-800 hover:bg-gray-700 text-xl text-white py-2 px-4 rounded"
-        >
-          More Insights
-        </button>
-      </div>
-    ) : ""}
-    {isOpen && (
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-        <div className="bg-gray-800 p-6 rounded-lg shadow-lg text-white"> {/* Dark background and white text */}
-          <button onClick={() => setIsOpen(false)} className="absolute top-2 right-2 text-gray-300 hover:text-gray-100">
-            Close
-          </button>
-          <div>Original dialog: {response.original_quote}</div>
-          <div>Character: {response.character}</div>
-          <div>Movie: {response.movie}</div>
-          <div>Year: {response.year}</div>
-          <div>Justifications:</div>
+        <div className="mt-24 text-lg font-normal">
+          <div><span className="font-bold text-gray-400">Original dialog: </span>{response.original_quote}</div>
+          <div><span className="font-bold text-gray-400">Character: </span>{response.character}</div>
+          <div><span className="font-bold text-gray-400">Movie: </span>{response.movie}</div>
+          <div><span className="font-bold text-gray-400">Year: </span>{response.year}</div>
+          <div><span className="font-bold text-gray-400">Justifications:</span></div>
           {response && response.options && response.options.map(({title, justification, is_chosen}, i) => (
             <div key={i}><b>{title}: </b>{justification}{is_chosen && " | CHOSEN!!!"}</div>
           ))}
         </div>
       </div>
-    )}
+    ) : ""}
     </div>
   );
 }
