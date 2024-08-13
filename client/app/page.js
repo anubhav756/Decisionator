337bf6b0282d07937851c6a307ab60bf0cf02a6a
Anubhav Dhawan <anubhavdhawan@google.com>
2024-08-13 12:10:33 +0530

    Improve submit button

--- client/app/page.js
+++ client/app/page.js
@@ -6,19 +6,28 @@ import styles from './page.module.css';
 const INITIAL_INPUT = '';
 const INITIAL_OPTIONS = [];
 const INITIAL_RESPONSE = {};
+const INITIAL_IS_SUBMITTING = false;
 
 export default function Page() {
   const [inputText, setInputText] = useState(INITIAL_INPUT);
   const [options, setOptions] = useState(INITIAL_OPTIONS);
   const [response, setResponse] = useState(INITIAL_RESPONSE);
+  const [isSubmitting, setIsSubmitting] = useState(INITIAL_IS_SUBMITTING);
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
   };
 
-  const handleSubmit = async () => {
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSnap();
+    }
+  };
+
+  const handleSnap = async () => {
     setResponse(INITIAL_RESPONSE);
     setOptions(INITIAL_OPTIONS);
+    setIsSubmitting(true);
     try {
       const response = await fetch('http://localhost:8000/ask', {
         method: 'POST',
@@ -42,6 +51,9 @@ export default function Page() {
     } catch (error) {
       console.error('Error:', error);
     }
+    finally {
+      setIsSubmitting(INITIAL_IS_SUBMITTING);
+    }
   };
 
   return (
@@ -50,18 +62,27 @@ export default function Page() {
         <input
           value={inputText}
           onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
           placeholder="Type here to decide..."
           className="w-3/4 p-8 rounded-lg outline-none bg-gray-900 text-white text-5xl resize-none"
         />
-      <div className="relative inline-flex group ml-8">
-        <div className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt" />
-        <a
-          onClick={handleSubmit}
-          className="relative inline-flex items-center justify-center px-8 py-4 text-5xl font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-          role="button">
-          Snap
-        </a>
-      </div>
+    <div className="relative inline-flex group ml-8">
+      <div className={`absolute transitiona-all duration-1000 bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt ${isSubmitting ? "opacity-100 -inset-1" : "opacity-70 -inset-px"}`} />
+      <a
+        onClick={(e) => {
+          if (isSubmitting || !inputText.length) {
+            e.preventDefault(); 
+          } else {
+            handleSnap(); 
+          }
+        }}
+        className={`relative inline-flex items-center justify-center px-8 py-4 text-5xl font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 focus:outline-none
+
+                   ${isSubmitting ? 'pointer-events-none opacity-50' : ''}`}
+        role="button">
+        Snap
+      </a>
+    </div>
     </div>
     <div>
       <div className="flex justify-center items-center text-4xl mt-24"> 
